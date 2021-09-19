4602c6443217bea401e52d1cefc90b7102d9ebc1
Maggie <63321497+xmaggieliu@users.noreply.github.com>
2021-09-18 23:31:02 -0400

    new google cloud stuff

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
-from flask import Flask, render_template, request, redirect, session, jsonify
+# app.py
+
+# Required imports
+import os
+from flask import Flask, request, jsonify
 from firebase_admin import credentials, firestore, initialize_app
 
+# Initialize Flask app
 app = Flask(__name__)
-
-app.config["TEMPLATES_AUTO_RELOAD"] = True
-
-@app.route("/", methods=["GET", "POST"])
-def index():
-    return render_template("index.html")
-
 
 # Initialize Firestore DB
 cred = credentials.Certificate('key.json')
@@ -76,7 +74,6 @@ def delete():
     except Exception as e:
         return f"An Error Occured: {e}"
 
-
-
+port = int(os.environ.get('PORT', 8080))
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=80)
+    app.run(threaded=True, host='0.0.0.0', port=port)
